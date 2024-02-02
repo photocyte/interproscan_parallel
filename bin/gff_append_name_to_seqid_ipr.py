65ad3ca6bbce8a01b7d5c36417aec6be9401732c
Tim Fallon <tfallon@ucsd.edu>
2024-02-01 18:29:24 -0800

    spin out gff_append_name_to_seqid_ipr into standalone python script

---
+++ bin/gff_append_name_to_seqid_ipr.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import re
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("gff")
+args = parser.parse_args()
+print(args.echo)
+
+os.mkdir('results')
+
+##Below targets no longer used. It is assumed everything will be renamed.
+##Check git if wanting to revert the behavior
+##targets = ["Gene3D","SUPERFAMILY"] ## Where the rename targets are set. Others are left untouched.
+
+r_handle = open(args.gff)
+w_handle = open(f'results/{args.gff}','w')
+for l in r_handle.readlines():
+    if l[0] == "#":
+        w_handle.write(l)
+        continue
+    NAME_MATCH = re.search("Name=([^;$]+)",l)
+    if NAME_MATCH != None:
+        NAME=NAME_MATCH.group(1)
+    else:
+        w_handle.write(l)
+        continue
+    SEQID = l.split("\\t")[0]
+    new_l = re.sub('match\\$[0-9]+_',SEQID+"__"+NAME+"_",l) ## Unclear what the numbers after match$ mean, so will delete.
+    w_handle.write(new_l)
+r_handle.close()
+w_handle.close()
--- interproscan_parallel.nf
+++ interproscan_parallel.nf
@@ -153,31 +153,7 @@ output:
  path "results/${gff}"
 shell:
 '''
-#!/usr/bin/env python
-import re
-import os
-os.mkdir('results')
-##Below targets no longer used. It is assumed everything will be renamed.
-##Check git if wanting to revert the behavior
-##targets = ["Gene3D","SUPERFAMILY"] ## Where the rename targets are set. Others are left untouched.
-
-r_handle = open('!{gff}')
-w_handle = open('results/!{gff}','w')
-for l in r_handle.readlines():
-    if l[0] == "#":
-        w_handle.write(l)
-        continue
-    NAME_MATCH = re.search("Name=([^;$]+)",l)
-    if NAME_MATCH != None:
-        NAME=NAME_MATCH.group(1)
-    else:
-        w_handle.write(l)
-        continue
-    SEQID = l.split("\\t")[0]
-    new_l = re.sub('match\\$[0-9]+_',SEQID+"__"+NAME+"_",l) ## Unclear what the numbers after match$ mean, so will delete.
-    w_handle.write(new_l)
-r_handle.close()
-w_handle.close()
+gff_append_name_to_seqid_ipr.py !{gff}
 '''
 }
 
