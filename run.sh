e8ae3bd67781c9de5222abed8f9cc9195f999107
Tim Fallon <tfallon@ucsd.edu>
2024-04-13 02:17:40 -0700

    improve run script

--- run.sh
+++ run.sh
@@ -11,5 +11,5 @@ NF_SCR="https://github.com/photocyte/interproscan_parallel"
 #nextflow run ${NF_SCR} -latest -resume -entry download_data ## Used to download the interproscan docker data thingy
 
 ## Running the InterProScan annotation and graphical plotting for the peptide FASTA of the 3 PKZILLAs:
-nextflow run ${NF_SCR} -r main -latest -resume --fasta PKZILLA_12B1_v1.1.gff3__12B1_scaffolds_v1.1.fasta.pep.fa --renaming renaming.tsv
+nextflow run ${NF_SCR} -r main -latest -resume --fasta PKZILLA-1-on-B.loci.nt.rename.consensus.fa.pep.fa --renaming ./dataset/renaming.tsv
 
