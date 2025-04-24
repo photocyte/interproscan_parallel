d3c42fdb975a4be70e969934c8c7b86093bebd47
Timothy R. Fallon <tfallon@ucsd.edu>
2025-04-24 13:06:48 -0700

    adapt to use singularity again

--- run.sh
+++ run.sh
@@ -1,15 +1,17 @@
 #NF_SCR="https://github.com/photocyte/interproscan_parallel"
 NF_SCR="$(pwd)/interproscan_parallel.nf"
 
-## Downloading the accessory data for Docker/Singularity execution of InterProScan
+## Downloading the accessory data for Docker/Singularity/Apptainer execution of InterProScan
 ## This ends up in the dependencies/download_interproscan_docker_data/ folder.
 ## You might have to modify the download_interproscan_docker_data process, and the nextflow.config singularity.runOptions to get it to work.
-##mkdir -p dependencies/download_interproscan_docker_data/interproscan-5.70-102.0/
-#mkdir -p apptainer
-#export APPTAINER_CACHEDIR="$(pwd)/apptainer"
-#export APPTAINER_TMPDIR="$(pwd)/apptainer"
-#export TMPDIR="$(pwd)/apptainer"
-#nextflow run ${NF_SCR} -c download.config -latest -resume -entry download_data ## Used to download the interproscan docker data thingy
+##mkdir -p dependencies/download_interproscan_docker_data/interproscan-5.73-104.0/
+#mkdir -p container_executor_cache
+#export APPTAINER_CACHEDIR="$(pwd)/container_executor_cache"
+#export APPTAINER_TMPDIR="$(pwd)/container_executor_cache"
+#export SINGULARITY_CACHEDIR="$(pwd)/container_executor_cache"
+#export SINGULARITY_TMPDIR="$(pwd)/container_executor_cache"
+#export TMPDIR="$(pwd)/container_executor_cache"
+#nextflow run ${NF_SCR} -c download.config -latest -resume -entry download_data ## Used to download the interproscan docker/singularity dependency data (HMMs, etc.)
 
 ## Running the InterProScan annotation and graphical plotting for the peptide FASTA of the 3 PKZILLAs:
 nextflow run ${NF_SCR} -r main -latest -resume --fasta PKZILLA-1-on-B.loci.nt.rename.consensus.fa.pep.fa --renaming ./dataset/renaming.tsv
