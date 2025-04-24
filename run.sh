1af2c3979f3583b25b196a0f823eb86877d9f86c
Timothy R. Fallon <tfallon@ucsd.edu>
2025-04-24 14:59:56 -0700

    accessory data symlinking instructions

--- run.sh
+++ run.sh
@@ -13,6 +13,12 @@ NF_SCR="$(pwd)/interproscan_parallel.nf"
 #export TMPDIR="$(pwd)/container_executor_cache"
 #nextflow run ${NF_SCR} -c conf/download.config -latest -resume -entry download_data ## Used to download the interproscan docker/singularity dependency data (HMMs, etc.)
 
+## An example of how to symlink the accessory data, if interproscan_parallel was run as a subdirectory of the parent directory:
+#mkdir -p dependencies/download_interproscan_docker_data/interproscan-5.73-104.0/
+#cd dependencies/download_interproscan_docker_data/interproscan-5.73-104.0/
+#ln -s ../../../../interproscan_parallel/dependencies/download_interproscan_docker_data/interproscan-5.73-104.0/data/ .
+#cd ../../../
+
 ## Running the InterProScan annotation and graphical plotting for the peptide FASTA of the 3 PKZILLAs:
 nextflow run ${NF_SCR} -r main -latest -resume --fasta PKZILLA-1-on-B.loci.nt.rename.consensus.fa.pep.fa --renaming ./dataset/renaming.tsv
 
